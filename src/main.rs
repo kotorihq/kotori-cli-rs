c8f27d88c6a5ccf20c59a8a41ec8215f3e2701bc
Martin Pešek <pesek.dev@gmail.com>
2018-03-19 02:31:24 +0100

    Add new command project/delete

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,6 @@ use reqwest::{Client, StatusCode};
 use reqwest::header::UserAgent;
 use std::collections::HashMap;
 use std::error::Error;
-use std::io::Read;
 use url::Url;
 
 header! { (XMasterKey, "x-master-key") => [String] }
@@ -94,15 +93,39 @@ fn project_create_handle_response(response: &mut reqwest::Response) -> Result<()
     Ok(())
 }
 
+fn project_delete(project_id: &str, server: &str, master_key: &str) -> Result<(), Box<Error>> {
+    println!("Using Kotori endpoint: {}", server);
+
+    let url = Url::parse(server)?.join("/api/projects/")?.join(project_id)?;
+
+    let mut params = HashMap::new();
+    params.insert("name", project_id);
+
+    let mut response = Client::new()
+        .delete(url)
+        .header(UserAgent::new("kotori-cli"))
+        .header(XMasterKey(master_key.to_owned()))
+        .send()?;
+
+    return handle_response(&mut response, &project_delete_handle_response);
+}
+
+fn project_delete_handle_response(response: &mut reqwest::Response) -> Result<(), Box<Error>> {
+    println!("Project deleted.");
+    Ok(())
+}
+
 fn handle_response(response: &mut reqwest::Response, f: &Fn(&mut reqwest::Response) -> Result<(), Box<Error>>) -> Result<(), Box<Error>> {
     match response.status() {
         StatusCode::Ok |
-        StatusCode::Created => {
+        StatusCode::Created |
+        StatusCode::NoContent => {
             return f(response);
         }
 
         StatusCode::Unauthorized |
         StatusCode::Forbidden |
+        StatusCode::NotFound |
         StatusCode::InternalServerError => {
             let error_response: ErrorResponse = response.json()?;
             println!("Error: {}", error_response.message);
@@ -134,7 +157,15 @@ fn dispatch(matches: &clap::ArgMatches) -> Result<(), Box<Error>> {
                 return project_create(name, server, master_key);
             }
 
-            Ok(())
+            if let Some(matches) = project_matches.subcommand_matches("delete") {
+                let project_id = matches.value_of("PROJECT ID").unwrap();
+                let server = matches.value_of("SERVER").unwrap();
+                let master_key = matches.value_of("master-key").unwrap();
+
+                return project_delete(project_id, server, master_key);
+            }
+
+            panic!("Should not have been called.");
         }
 
         _ => { panic!("Should not have been called."); }
@@ -173,6 +204,22 @@ fn main() {
                     .index(1))
                 .arg(Arg::with_name("NAME")
                     .help("Project name")
+                    .required(true)
+                    .index(2)))
+            .subcommand(SubCommand::with_name("delete")
+                .about("Delete a project")
+                .arg(Arg::with_name("master-key")
+                    .help("Sets master key")
+                    .long("master-key")
+                    .required(true)
+                    .value_name("key")
+                    .takes_value(true))
+                .arg(Arg::with_name("SERVER")
+                    .help("Kotori server endpoint")
+                    .required(true)
+                    .index(1))
+                .arg(Arg::with_name("PROJECT ID")
+                    .help("Project ID")
                     .required(true)
                     .index(2))))
         .get_matches();
